bd7f66997adcb1fe58c374e20f531d5be04f7e70
udiptaB <47241853+udiptaB@users.noreply.github.com>
2025-09-16 13:50:15 +0530

    Create q5_Monthly_Transactions_1_U_soln.sql

---
+++ q5_Monthly_Transactions_1_U_soln.sql
@@ -0,0 +1,8 @@
+SELECT TO_CHAR(trans_date,'YYYY-MM' ) AS month,
+    country,COUNT(amount) AS trans_count,
+    COUNT(CASE WHEN state='approved' THEN amount END) AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM(CASE WHEN state='approved' THEN amount ELSE 0 END) AS approved_total_amount
+ FROM transactions
+ GROUP BY country,month
+ ORDER BY month
